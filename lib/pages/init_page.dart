bb635c77f60d756429a691390c820671d655f8a5
MinJaehyun <43669992+MinJaehyun@users.noreply.github.com>
2023-06-03 01:18:40 +0900

    중복 코드 개선하기 전

--- lib/pages/init_page.dart
+++ lib/pages/init_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class InitPage extends StatelessWidget {
   const InitPage({Key? key}) : super(key: key);
@@ -6,44 +7,162 @@ class InitPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: Drawer(
-        child: ListView(
-          children: [
-            DrawerHeader(
-              child: IconButton(
-                onPressed: () {},
-                icon: IconButton(
-                    onPressed: () {
-                      Navigator.of(context).pop();
-                    },
-                    icon: Icon(Icons.close)),
-              ),
-            ),
-          ],
-        ),
-      ),
-      appBar: AppBar(
-        title: Text('Portfolio'),
-        centerTitle: true,
-      ),
       body: Container(
-        child: Row(
-          children: [
-            // 3개의 컨테이너 비율 지정하기
-            // 비율 지정하는 방법 ? Expanded?? , flex
-            Flexible(
-              flex: 1,
-              child: Container(color: Colors.blueAccent),
-            ),
-            Flexible(
-              flex: 3,
-              child: Container(color: Colors.greenAccent),
-            ),
-            Flexible(
-              flex: 6,
-              child: Container(color: Colors.redAccent),
-            ),
-          ],
+        child: LayoutBuilder(
+          builder: (context, constraints) {
+            // note: mobile_body
+            if (constraints.maxWidth < 1400) {
+              return Row(
+                children: [
+                  Flexible(
+                    fit: FlexFit.tight,
+                    child: Container(
+                      margin: EdgeInsets.all(12.0),
+                      padding: EdgeInsets.all(20.0),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          // note: 길이 확인: Text(constraints.maxWidth.toString()),
+                          Column(
+                            children: [
+                              IconButton(
+                                onPressed: () {},
+                                icon: Icon(Icons.perm_identity_outlined),
+                              ),
+                            ],
+                          ),
+                          Column(
+                            children: [
+                              IconButton(
+                                onPressed: () {},
+                                icon: Icon(Icons.build_outlined),
+                              ),
+                            ],
+                          ),
+                          Column(
+                            children: [
+                              IconButton(
+                                onPressed: () {},
+                                icon: Icon(
+                                  Icons.note_outlined,
+                                ),
+                              ),
+                            ],
+                          ),
+                          Column(
+                            children: [
+                              IconButton(
+                                onPressed: () {},
+                                icon: Icon(
+                                  Icons.format_list_numbered_rtl_outlined,
+                                ),
+                              ),
+                            ],
+                          ),
+                          Column(
+                            children: [
+                              IconButton(
+                                onPressed: () {},
+                                icon: SvgPicture.asset('assets/images/left_list/github-5.svg'),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Flexible(
+                    flex: 11,
+                    child: Container(
+                      margin: EdgeInsets.all(12.0),
+                      padding: EdgeInsets.all(20.0),
+                      decoration: BoxDecoration(
+                        color: Colors.greenAccent,
+                      ),
+                    ),
+                  ),
+                ],
+              );
+            }
+            // note: desktop_body
+            else {
+              return Row(
+                children: [
+                  Flexible(
+                    fit: FlexFit.tight,
+                    child: Container(
+                      margin: EdgeInsets.all(12.0),
+                      padding: EdgeInsets.all(20.0),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          Column(
+                            children: [
+                              IconButton(
+                                onPressed: () {},
+                                icon: Icon(Icons.perm_identity_outlined),
+                              ),
+                              Text('ABOUT'),
+                            ],
+                          ),
+                          Column(
+                            children: [
+                              IconButton(
+                                onPressed: () {},
+                                icon: Icon(Icons.build_outlined),
+                              ),
+                              Text('WORKS'),
+                            ],
+                          ),
+                          Column(
+                            children: [
+                              IconButton(
+                                onPressed: () {},
+                                icon: Icon(
+                                  Icons.note_outlined,
+                                ),
+                              ),
+                              Text('BLOG'),
+                            ],
+                          ),
+                          Column(
+                            children: [
+                              IconButton(
+                                onPressed: () {},
+                                icon: Icon(
+                                  Icons.format_list_numbered_rtl_outlined,
+                                ),
+                              ),
+                              Text('NOTION'),
+                            ],
+                          ),
+                          Column(
+                            children: [
+                              IconButton(
+                                onPressed: () {},
+                                icon: SvgPicture.asset('assets/images/left_list/github-5.svg'),
+                              ),
+                              Text('GITHUB'),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+
+                  // note: 우측 컨테이너
+                  Flexible(
+                    flex: 11,
+                    child: Container(
+                      margin: EdgeInsets.all(12.0),
+                      padding: EdgeInsets.all(20.0),
+                      color: Colors.greenAccent,
+                    ),
+                  ),
+                ],
+              );
+            }
+          },
         ),
       ),
     );
