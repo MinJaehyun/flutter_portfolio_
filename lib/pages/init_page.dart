42d65ee1bafe02dd218d89a9c2f2d1c89041539a
MinJaehyun <43669992+MinJaehyun@users.noreply.github.com>
2023-06-03 01:55:39 +0900

    중복된 코드 개선함

--- lib/pages/init_page.dart
+++ lib/pages/init_page.dart
@@ -10,158 +10,89 @@ class InitPage extends StatelessWidget {
       body: Container(
         child: LayoutBuilder(
           builder: (context, constraints) {
-            // note: mobile_body
-            if (constraints.maxWidth < 1400) {
-              return Row(
-                children: [
-                  Flexible(
-                    fit: FlexFit.tight,
-                    child: Container(
-                      margin: EdgeInsets.all(12.0),
-                      padding: EdgeInsets.all(20.0),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        children: [
-                          // note: 길이 확인: Text(constraints.maxWidth.toString()),
-                          Column(
-                            children: [
-                              IconButton(
-                                onPressed: () {},
-                                icon: Icon(Icons.perm_identity_outlined),
+            return Row(
+              children: [
+                Flexible(
+                  fit: FlexFit.tight,
+                  child: Container(
+                    margin: EdgeInsets.all(12.0),
+                    padding: EdgeInsets.all(20.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        // note: 길이 확인: Text(constraints.maxWidth.toString()),
+                        Column(
+                          children: [
+                            IconButton(
+                              onPressed: () {},
+                              icon: Icon(Icons.perm_identity_outlined),
+                            ),
+                            if (constraints.maxWidth > 1400) Text('ABOUT'),
+                          ],
+                        ),
+                        Column(
+                          children: [
+                            IconButton(
+                              onPressed: () {},
+                              icon: Icon(Icons.build_outlined),
+                            ),
+                            if (constraints.maxWidth > 1400) Text('WORKS'),
+                          ],
+                        ),
+                        Column(
+                          children: [
+                            IconButton(
+                              onPressed: () {},
+                              icon: Icon(
+                                Icons.note_outlined,
                               ),
-                            ],
-                          ),
-                          Column(
-                            children: [
-                              IconButton(
-                                onPressed: () {},
-                                icon: Icon(Icons.build_outlined),
+                            ),
+                            if (constraints.maxWidth > 1400) Text('BLOG'),
+                          ],
+                        ),
+                        Column(
+                          children: [
+                            IconButton(
+                              onPressed: () {},
+                              icon: Icon(
+                                Icons.format_list_numbered_rtl_outlined,
                               ),
-                            ],
-                          ),
-                          Column(
-                            children: [
-                              IconButton(
-                                onPressed: () {},
-                                icon: Icon(
-                                  Icons.note_outlined,
-                                ),
-                              ),
-                            ],
-                          ),
-                          Column(
-                            children: [
-                              IconButton(
-                                onPressed: () {},
-                                icon: Icon(
-                                  Icons.format_list_numbered_rtl_outlined,
-                                ),
-                              ),
-                            ],
-                          ),
-                          Column(
-                            children: [
-                              IconButton(
-                                onPressed: () {},
-                                icon: SvgPicture.asset('assets/images/left_list/github-5.svg'),
-                              ),
-                            ],
-                          ),
-                        ],
-                      ),
+                            ),
+                            if (constraints.maxWidth > 1400) Text('NOTION'),
+                          ],
+                        ),
+                        Column(
+                          children: [
+                            IconButton(
+                              onPressed: () {},
+                              icon: SvgPicture.asset('assets/images/left_list/github-5.svg'),
+                            ),
+                            if (constraints.maxWidth > 1400) Text('GITHUB'),
+                          ],
+                        ),
+                        Column(
+                          children: [
+                            IconButton(
+                              onPressed: () {},
+                              icon: Icon(Icons.contact_mail_outlined),
+                            ),
+                            if (constraints.maxWidth > 1400) Text('CONTACT'),
+                          ],
+                        ),
+                      ],
                     ),
                   ),
-                  Flexible(
-                    flex: 11,
-                    child: Container(
-                      margin: EdgeInsets.all(12.0),
-                      padding: EdgeInsets.all(20.0),
-                      decoration: BoxDecoration(
-                        color: Colors.greenAccent,
-                      ),
-                    ),
+                ),
+                Flexible(
+                  flex: 11,
+                  child: Container(
+                    margin: EdgeInsets.all(12.0),
+                    padding: EdgeInsets.all(20.0),
+                    decoration: BoxDecoration(color: Colors.greenAccent),
                   ),
-                ],
-              );
-            }
-            // note: desktop_body
-            else {
-              return Row(
-                children: [
-                  Flexible(
-                    fit: FlexFit.tight,
-                    child: Container(
-                      margin: EdgeInsets.all(12.0),
-                      padding: EdgeInsets.all(20.0),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        children: [
-                          Column(
-                            children: [
-                              IconButton(
-                                onPressed: () {},
-                                icon: Icon(Icons.perm_identity_outlined),
-                              ),
-                              Text('ABOUT'),
-                            ],
-                          ),
-                          Column(
-                            children: [
-                              IconButton(
-                                onPressed: () {},
-                                icon: Icon(Icons.build_outlined),
-                              ),
-                              Text('WORKS'),
-                            ],
-                          ),
-                          Column(
-                            children: [
-                              IconButton(
-                                onPressed: () {},
-                                icon: Icon(
-                                  Icons.note_outlined,
-                                ),
-                              ),
-                              Text('BLOG'),
-                            ],
-                          ),
-                          Column(
-                            children: [
-                              IconButton(
-                                onPressed: () {},
-                                icon: Icon(
-                                  Icons.format_list_numbered_rtl_outlined,
-                                ),
-                              ),
-                              Text('NOTION'),
-                            ],
-                          ),
-                          Column(
-                            children: [
-                              IconButton(
-                                onPressed: () {},
-                                icon: SvgPicture.asset('assets/images/left_list/github-5.svg'),
-                              ),
-                              Text('GITHUB'),
-                            ],
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-
-                  // note: 우측 컨테이너
-                  Flexible(
-                    flex: 11,
-                    child: Container(
-                      margin: EdgeInsets.all(12.0),
-                      padding: EdgeInsets.all(20.0),
-                      color: Colors.greenAccent,
-                    ),
-                  ),
-                ],
-              );
-            }
+                ),
+              ],
+            );
           },
         ),
       ),
