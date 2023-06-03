ecf104bce73b10cf4746b98567e49c881d98282a
MinJaehyun <43669992+MinJaehyun@users.noreply.github.com>
2023-06-04 03:09:50 +0900

    전체 페이지에 폰트 적용

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      // note: 폰트를 전체 앱에 적용시키기 위한 설정: ThemeData 의 fontFamily 속성 추가
+      theme: ThemeData(fontFamily: 'Aggro'),
+      themeMode: ThemeMode.system,
       title: 'Portfolio',
       initialRoute: '/',
       routes: {
