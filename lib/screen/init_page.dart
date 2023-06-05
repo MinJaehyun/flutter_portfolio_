d634716568ac4aac2efba0b9e6c2547202e76079
MinJaehyun <43669992+MinJaehyun@users.noreply.github.com>
2023-06-05 15:13:59 +0900

    좌측 패널 임펙트 효과 추가

--- lib/screen/init_page.dart
+++ lib/screen/init_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_portfolio/screen/about_list.dart';
 import 'package:flutter_portfolio/screen/resume_page.dart';
-
+import 'package:animated_text_kit/animated_text_kit.dart';
 
 class InitPage extends StatefulWidget {
   const InitPage({Key? key}) : super(key: key);
@@ -50,7 +50,16 @@ class _InitPageState extends State<InitPage> {
                       onPressed: () => setState(() => mainPage = 'about'),
                       icon: Icon(Icons.perm_identity_outlined),
                     ),
-                    if (constraints.maxWidth > 1400) Text('ABOUT'),
+                    // note: 1400 이상이면서(&&), 클릭한 대상이면 애니메이션 적용하여 text 나타내기
+                    if ((constraints.maxWidth > 1400) && (mainPage == 'about'))
+                      AnimatedTextKit(
+                        animatedTexts: [
+                          WavyAnimatedText('ABOUT'),
+                        ],
+                        repeatForever: true,
+                      )
+                      // note: 크기가 1400 이상이고, 클릭한 대상이 아니면 text 만 나타내기
+                    else if (constraints.maxWidth > 1400) Text('ABOUT')
                   ],
                 ),
                 // note: 이력서
@@ -62,7 +71,14 @@ class _InitPageState extends State<InitPage> {
                         Icons.note_outlined,
                       ),
                     ),
-                    if (constraints.maxWidth > 1400) Text('RESUME'),
+                    if ((constraints.maxWidth > 1400) && (mainPage == 'resume'))
+                      AnimatedTextKit(
+                        animatedTexts: [
+                          WavyAnimatedText('RESUME'),
+                        ],
+                        repeatForever: true,
+                      )
+                    else if (constraints.maxWidth > 1400) Text('RESUME')
                   ],
                 ),
                 Column(
@@ -71,7 +87,14 @@ class _InitPageState extends State<InitPage> {
                       onPressed: () => setState(() => mainPage = 'works'),
                       icon: Icon(Icons.build_outlined),
                     ),
-                    if (constraints.maxWidth > 1400) Text('WORKS'),
+                    if ((constraints.maxWidth > 1400) && (mainPage == 'works'))
+                      AnimatedTextKit(
+                        animatedTexts: [
+                          WavyAnimatedText('WORKS'),
+                        ],
+                        repeatForever: true,
+                      )
+                    else if (constraints.maxWidth > 1400) Text('WORKS')
                   ],
                 ),
                 Column(
@@ -80,16 +103,30 @@ class _InitPageState extends State<InitPage> {
                       onPressed: () => setState(() => mainPage = 'github'),
                       icon: Icon(Icons.account_circle_outlined),
                     ),
-                    if (constraints.maxWidth > 1400) Text('GITHUB'),
+                    if ((constraints.maxWidth > 1400) && (mainPage == 'github'))
+                      AnimatedTextKit(
+                        animatedTexts: [
+                          WavyAnimatedText('GITHUB'),
+                        ],
+                        repeatForever: true,
+                      )
+                    else if (constraints.maxWidth > 1400) Text('GITHUB')
                   ],
                 ),
                 Column(
                   children: [
                     IconButton(
-                      onPressed: () => setState(() => mainPage = 'about'),
+                      onPressed: () => setState(() => mainPage = 'contact'),
                       icon: Icon(Icons.contact_mail_outlined),
                     ),
-                    if (constraints.maxWidth > 1400) Text('CONTACT'),
+                    if ((constraints.maxWidth > 1400) && (mainPage == 'contact'))
+                      AnimatedTextKit(
+                        animatedTexts: [
+                          WavyAnimatedText('CONTACT'),
+                        ],
+                        repeatForever: true,
+                      )
+                    else if (constraints.maxWidth > 1400) Text('CONTACT')
                   ],
                 ),
               ],
@@ -99,7 +136,7 @@ class _InitPageState extends State<InitPage> {
         // note: 클릭한 대상을 넘겨받고, 이를 변수에 담아 아래 컨테이너에 담고 처리한다
         // note: if(mainPage == 'about') 이면 about_list.dart 가져오기
         if (mainPage == 'about') AboutList(),
-        if(mainPage == 'resume') ResumePage(),
+        if (mainPage == 'resume') ResumePage(),
         // if(mainPage == 'works') WorksList(),
         // if(mainPage == 'github') GithubPage(),
         // if(mainPage == 'contact') ContactPage(),
