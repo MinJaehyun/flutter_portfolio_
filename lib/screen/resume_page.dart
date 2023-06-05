8c7bb6e7866f54dd76441a25a24a68c93a9afc5c
MinJaehyun <43669992+MinJaehyun@users.noreply.github.com>
2023-06-05 17:40:20 +0900

    introduction_screen page 구현

--- lib/screen/resume_page.dart
+++ lib/screen/resume_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:introduction_screen/introduction_screen.dart';
 
 class ResumePage extends StatefulWidget {
   const ResumePage({Key? key}) : super(key: key);
@@ -8,19 +9,77 @@ class ResumePage extends StatefulWidget {
 }
 
 class _ResumePageState extends State<ResumePage> {
+  final introKey = GlobalKey<IntroductionScreenState>();
 
   @override
   Widget build(BuildContext context) {
+    const bodyStyle = TextStyle(fontSize: 19.0);
+    const pageDecoration = PageDecoration(
+      titleTextStyle: TextStyle(fontSize: 28.0, fontWeight: FontWeight.w700),
+      bodyTextStyle: bodyStyle,
+      bodyPadding: EdgeInsets.fromLTRB(16.0, 0.0, 16.0, 16.0),
+      pageColor: Colors.white,
+      imagePadding: EdgeInsets.zero,
+    );
+
+    Widget _buildFullscreenImage(String assetName) {
+      return Image.asset(
+        'assets/resume_image/$assetName',
+        fit: BoxFit.cover,
+        width: 650.0,
+        alignment: Alignment.center,
+      );
+    }
+
     return Flexible(
       flex: 11,
       fit: FlexFit.tight,
       child: Container(
-        width: 1500,
+        // width: 1000,
         height: double.infinity,
         margin: EdgeInsets.all(12.0),
         padding: EdgeInsets.all(20.0),
         // decoration: BoxDecoration(color: Colors.green),
-        child: Text('test resume page'),
+        child: IntroductionScreen(
+          key: introKey,
+          // globalBackgroundColor: Colors.white,
+          allowImplicitScrolling: true,
+          // autoScrollDuration: 3000,
+          done: const Text('Done', style: TextStyle(fontWeight: FontWeight.w600)),
+          onDone: (){
+            // todo: 완료 시, 어떡할건지 ?
+          },
+          next: const Icon(Icons.arrow_forward),
+          pages: [
+            PageViewModel(
+              title: "",
+              body: "",
+              image: _buildFullscreenImage('mjh_resume_1.png'),
+              decoration: pageDecoration.copyWith(
+                contentMargin: const EdgeInsets.symmetric(horizontal: 16),
+                fullScreen: true,
+              ),
+            ),
+            PageViewModel(
+              title: "",
+              body: "",
+              image: _buildFullscreenImage('mjh_resume_2.png'),
+              decoration: pageDecoration.copyWith(
+                contentMargin: const EdgeInsets.symmetric(horizontal: 16),
+                fullScreen: true,
+              ),
+            ),
+            PageViewModel(
+              title: "",
+              body: "",
+              image: _buildFullscreenImage('mjh_resume_3.png'),
+              decoration: pageDecoration.copyWith(
+                contentMargin: const EdgeInsets.symmetric(horizontal: 16),
+                fullScreen: true,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
