2aa8976ff2f4c3834d94313452ec3549a05dd594
MinJaehyun <43669992+MinJaehyun@users.noreply.github.com>
2023-07-03 14:01:46 +0900

    앱 system 에 따라 dart/light mode 변경되는 themeMode system 기능 추가

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,16 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       // note: 폰트를 전체 앱에 적용시키기 위한 설정: ThemeData 의 fontFamily 속성 추가
-      theme: ThemeData(fontFamily: 'Aggro'),
+      theme: ThemeData(
+        fontFamily: 'Aggro',
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.white, brightness: Brightness.light),
+        useMaterial3: true,
+      ),
+      darkTheme: ThemeData(
+        fontFamily: 'Aggro',
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.black, brightness: Brightness.dark),
+        useMaterial3: true,
+      ),
       themeMode: ThemeMode.system,
       title: 'Portfolio',
       initialRoute: '/',
