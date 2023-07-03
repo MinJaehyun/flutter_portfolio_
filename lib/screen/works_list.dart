d05bdbef46e79f05b3500f340c6e54ff07311407
MinJaehyun <43669992+MinJaehyun@users.noreply.github.com>
2023-07-03 14:05:45 +0900

    모바일 화면 기능 개선 - WORKS

--- lib/screen/works_list.dart
+++ lib/screen/works_list.dart
@@ -3,20 +3,32 @@ import 'package:flutter_portfolio/screen/works_center_panel/biba.dart';
 import 'package:flutter_portfolio/screen/works_center_panel/django.dart';
 import 'package:flutter_portfolio/screen/works_center_panel/mongodb.dart';
 import 'package:flutter_portfolio/screen/works_center_panel/my_note.dart';
-// import 'package:animated_text_kit/animated_text_kit.dart';
 
 class WorksList extends StatefulWidget {
-  const WorksList({Key? key}) : super(key: key);
+  WorksList(this._selectedWork, {Key? key}) : super(key: key);
+  String _selectedWork;
 
   @override
   State<WorksList> createState() => _WorksListState();
 }
 
 class _WorksListState extends State<WorksList> {
-  var selectedWork = 'mongoDB';
+  // note: selectedWork 를 빈문자열 처리하면 4개의 프로젝트를 선택해야만 화면에 나타낼 수 있게 설정된다
+  // String selectedWork = '';
+  String selectedWork = 'mongoDB';
+
+  @override
+  void initState() {
+    super.initState();
+    setState(() {
+      selectedWork = widget._selectedWork;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
+    final _currentWidth = MediaQuery.of(context).size.width;
+
     List<String> works_image_left = [
       'assets/works_image_left/mongoDB.png',
       'assets/works_image_left/my_note.png',
@@ -28,66 +40,59 @@ class _WorksListState extends State<WorksList> {
       flex: 11,
       fit: FlexFit.tight,
       child: Container(
-          width: 1500,
-          height: double.infinity,
-          margin: EdgeInsets.all(12.0),
-          padding: EdgeInsets.all(20.0),
-          // decoration: BoxDecoration(color: Colors.green),
-          child: LayoutBuilder(
-            builder: (context, constraints) {
-              return Container(
-                child: Row(
-                  children: [
-                    // note: 좌측 패널
-                    Flexible(
-                      child: Container(
-                        padding: EdgeInsets.all(12),
-                        decoration: BoxDecoration(
-                          border: Border(
-                            left: BorderSide(color: Colors.grey, width: 1.0),
-                            // right: BorderSide(color: Colors.grey, width: 1.0),
-                          ),
-                          // color: Colors.blueAccent
-                        ),
-                        child: ListView.separated(
-                          separatorBuilder: (context, index) => Divider(height: 135, color: Colors.white),
-                          itemCount: works_image_left.length,
-                          itemBuilder: (context, index) {
-                            return SizedBox(
-                              height: 180,
-                              child: IconButton(
-                                onPressed: () {
-                                  setState(
-                                    () {
-                                      // print(works_image_left[index]); // 'assets/works_image_left/mongoDB.png' 값을 출력하는데, /끝에 mongoDB 만 받고 싶다
-                                      var selectWork = works_image_left[index].split('/')[2].split('.')[0];
-                                      selectedWork = selectWork;
+        width: 1500,
+        height: double.infinity,
+        margin: EdgeInsets.all(12.0),
+        padding: EdgeInsets.all(20.0),
+        // decoration: BoxDecoration(color: Colors.green),
+        child: LayoutBuilder(
+          builder: (context, constraints) {
+            return Container(
+              child: Row(
+                children: [
+                  // note: 좌측 패널
+                  Flexible(
+                    // note: works 클릭 시, _currentWidth > 960 이상만 보여주고 이하면 works 작업 내용 안 보인다
+                    child: _currentWidth > 960
+                        ? Container(
+                            padding: EdgeInsets.all(12),
+                            child: ListView.separated(
+                              separatorBuilder: (context, index) => Divider(height: 5, color: Colors.white),
+                              itemCount: works_image_left.length,
+                              itemBuilder: (context, index) {
+                                return SizedBox(
+                                  height: 180,
+                                  child: IconButton(
+                                    onPressed: () {
+                                      setState(() {
+                                        // print(works_image_left[index]);
+                                        // note: 'assets/works_image_left/mongoDB.png' 값 출력하는데, /끝에 mongoDB 만 받기 위한 작업
+                                        var selectWork = works_image_left[index].split('/')[2].split('.')[0];
+                                        selectedWork = selectWork;
+                                      });
                                     },
-                                  );
-                                },
-                                icon: Image.asset(
-                                  works_image_left[index],
-                                ),
-                              ),
-                            );
-                          },
-                        ),
-                      ),
-                    ),
-                    SizedBox(width: 15),
-                    // note: 우측 패널
-                    // my_note, django, biba
-                    if (selectedWork == 'mongoDB') MongoDB(),
-                    if (selectedWork == 'my_note') MyNote(),
-                    if (selectedWork == 'django') Django(),
-                    if (selectedWork == 'biba') Biba(),
-                  ],
-                ),
-              );
-            },
-          )),
+                                    icon: Image.asset(works_image_left[index]),
+                                  ),
+                                );
+                              },
+                            ),
+                          )
+                        // note: 삼항연산자 맞춰주기 위한 빈박스
+                        : SizedBox(),
+                  ),
+                  SizedBox(width: 15),
+                  // note: 우측 패널
+                  // my_note, django, biba
+                  if (selectedWork == 'mongoDB') MongoDB(),
+                  if (selectedWork == 'my_note') MyNote(),
+                  if (selectedWork == 'django') Django(),
+                  if (selectedWork == 'biba') Biba(),
+                ],
+              ),
+            );
+          },
+        ),
+      ),
     );
   }
 }
-
-// pub: https://pub.dev/packages/animated_text_kit
