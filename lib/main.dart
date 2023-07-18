37b95304ef250e58c9ab91006d6118460adfae12
MinJaehyun <43669992+MinJaehyun@users.noreply.github.com>
2023-07-18 15:35:22 +0900

    Refactor 색상 스키마를 파일로 저장하여 변경 및 노란 테마로 변경 #1

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_portfolio/helper/color_schemes.g.dart';
 import 'package:flutter_portfolio/screen/init_page.dart';
 
 void main() {
@@ -14,12 +15,15 @@ class MyApp extends StatelessWidget {
       // note: 폰트를 전체 앱에 적용시키기 위한 설정: ThemeData 의 fontFamily 속성 추가
       theme: ThemeData(
         fontFamily: 'Aggro',
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.white, brightness: Brightness.light),
+        // colorScheme: ColorScheme.fromSeed(seedColor: Colors.white, brightness: Brightness.light),
+        colorScheme: lightColorScheme,
         useMaterial3: true,
       ),
       darkTheme: ThemeData(
         fontFamily: 'Aggro',
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.black, brightness: Brightness.dark),
+        // 변경 전: colorScheme: ColorScheme.fromSeed(seedColor: Colors.black, brightness: Brightness.dark),
+        // note: 변경 후: 색상을 스키마로 설정하여 가져옴. 추 후 내맘대로 스키마를 변경할 수 있다
+        colorScheme: darkColorScheme,
         useMaterial3: true,
       ),
       themeMode: ThemeMode.system,
